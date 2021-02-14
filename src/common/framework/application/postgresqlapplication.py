e652ef9183e315ca746c3c0aea94e41c3f443c3e
moratori <hoko1993@yahoo.co.jp>
2021-02-15 00:31:32 +0900

    postgresqlとの接続に対応

---
+++ src/common/framework/application/postgresqlapplication.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import common.framework.application.baseapplication as appframe
+
+from logging import getLogger
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+LOGGER = getLogger(__name__)
+
+
+class PostgreSQLApplication(appframe.BaseApplication):
+
+    def __init__(self, module_name, script_name):
+        super().__init__(module_name, script_name)
+
+    def validate_config(self):
+        self.conf.common.postgresql.user
+        self.conf.common.postgresql.passwd
+        self.conf.common.postgresql.host
+        self.conf.common.postgresql.dbname
+
+    def setup_resource(self):
+        database_specifier = \
+            "postgresql+psycopg2://%s:%s@%s/%s" % (
+                self.conf.common.postgresql.user,
+                self.conf.common.postgresql.passwd,
+                self.conf.common.postgresql.host,
+                self.conf.common.postgresql.dbname)
+        self.dbengine = create_engine(database_specifier,
+                                      encoding="utf-8",
+                                      echo=False)
+
+        LOGGER.debug("create db engine completed")
+
+        session = scoped_session(sessionmaker(autocommit=False,
+                                              autoflush=False,
+                                              bind=self.dbengine))
+
+        LOGGER.debug("create scoped session completed")
+
+        self.session = session
+
+    def setup_application(self):
+        pass
+
+    def teardown_application(self):
+        pass
+
+    def teardown_resource(self):
+        self.session.close()
